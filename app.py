bc879746851a148ac464c9ec7ced09dbfa260dc0
shaqvendetta <32623432+shaqvendetta@users.noreply.github.com>
2018-02-04 21:59:40 -0600

    info3180-lab01

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -13,7 +13,13 @@ app = Flask(__name__)
 # Routing for your application.
 # Put your routes below this comment
 '''
-
+@app.route("/")
+def home():
+    return "My home page"
+    
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -22,4 +28,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host="0.0.0.0", port=8080)
